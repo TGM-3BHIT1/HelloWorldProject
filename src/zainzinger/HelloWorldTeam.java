6ffb83a1ba167307a1773534ffda17c70abc249e
Lukas Zainzinger <lzainzinger@student.tgm.ac.at>
2014-03-05 15:52:29 +0100

    First File

---
+++ src/zainzinger/HelloWorldTeam.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package zainzinger;
+
+/**
+ * @author lzainzinger
+ *
+ */
+public class HelloWorldTeam {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World! lzainzinger!");
+
+	}
+
+}
