615267daed9739454df047e8b9e9be3e7c8e53ca
tgm1314-skreutzer <skreutzer@student.tgm.ac.at>
2014-03-21 11:24:41 +0100

    Hallo

---
+++ src/kreutzer/HelloWorldTeam.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package kreutzer;
+
+/**
+ * @author sarahkreutzer
+ * @version 2014-03-21
+ * 
+ * Eine Klasse , die eine Methode mit einem Gruß enthält.
+ */
+public class HelloWorldTeam {
+	
+	public static void sarah(){
+			System.out.println("Schöne Grüße von Sarah");
+		}
+	/**
+	 * Das ist die Main-Methode ,die die Methode halloWorld aufruft.
+	 */
+	public static void main(String[] args) {
+		HelloWorldTeam.sarah();
+
+	}
+
+}
